ac3b0160aba509e21ec3f8d0ef78e82d9a870a76
Fuku-hiro112 <hirokazu.fukuda112@gmail.com>
2023-10-27 02:23:37 +0900

    プロトタイプ:WAVEシステム、ランダム生成完了s.met

--- Assets/Scripts/GardenGenerator.cs
+++ Assets/Scripts/GardenGenerator.cs
@@ -5,30 +5,34 @@ using Cysharp.Threading.Tasks;
 using static UnityEditor.PlayerSettings;
 using static UnityEditor.Searcher.SearcherWindow.Alignment;
 using System;
+using Unity.VisualScripting.Antlr3.Runtime;
+using System.Threading;
+using UnityEngine.UI;
 
 public class GardenGenerator : MonoBehaviour
 {
     private GameObject[] _gardenKinds;
     [SerializeField]private GameObject _prepareGarden;
     Vector3 _gardenPos = Vector3.zero;
-    [SerializeField] int _daysUpWave = 5;// waveが上がる日数
+    [SerializeField]private int _daysUpWave = 4;// waveが上がる日数
+    [SerializeField] private Text _dayText;
+    [SerializeField] private Text _waveText;
     private int _dayNum = 1;
     private int _waveNum = 1;
-    int idx = 0;
+    Transform _player;
 
     void Start()
     {
+        _dayText.text = $"{_dayNum}DAY";
+        _waveText.text = $"{_waveNum}WAVE";
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+        CancellationToken token = this.GetCancellationTokenOnDestroy();
+
         int childNum = transform.childCount;// 子オブジェクトの数
-        
         // 箱庭の種類を格納
         _gardenKinds = SetGardenKinds(childNum);
 
-
-
-        // 出現する箱庭を決定
-        List<GameObject> gardenSet = SetRandomGarden(childNum);
-
-
+        ControlGardenAsync(childNum, token).Forget();
 
         /*
         // 箱庭生成
@@ -49,6 +53,46 @@ public class GardenGenerator : MonoBehaviour
             _dayNum++;
             GenerateGarden(gardenSet);
         }*/
+    }
+    /// <summary>
+    /// 箱庭を出現させる
+    /// </summary>
+    /// <param name="childNum"></param>
+    /// <returns></returns>
+    private async UniTaskVoid ControlGardenAsync(int childNum, CancellationToken token)
+    {
+        while (true)
+        {
+            // 出現する箱庭を決定
+            GameObject[] gardens = SetRandomGarden(childNum);
+            // 箱庭生成
+            GenerateGarden(gardens);
+            await UniTask.WaitUntil(()=> _player.position.z >= _gardenPos.z, cancellationToken:token);
+            // dayによってwaveを増やす
+            if (_dayNum % _daysUpWave == 0) 
+            { 
+                _waveNum++;
+                _waveText.text = $"{_waveNum}WAVE";
+            }
+            _dayNum++;
+            _dayText.text = $"{_dayNum}DAY";
+        }
+    }
+    /// <summary>
+    /// 箱庭生成
+    /// </summary>
+    /// <param name="gardens"></param>
+    private void GenerateGarden(GameObject[] gardens)
+    {
+        foreach (GameObject garden in gardens)
+        {
+            // 出現位置を更新
+            float gardenScalZ = garden.transform.localScale.z * 10;// meshの大きさによって変わるので変更が必要
+            _gardenPos += new Vector3(0, 0, gardenScalZ);// 箱庭のスケール分足す
+
+            // 箱庭生成
+            Instantiate(garden,　_gardenPos,　Quaternion.Euler(Vector3.zero));
+        }
     }
 
     /// <summary>
@@ -75,131 +119,18 @@ public class GardenGenerator : MonoBehaviour
     /// </summary>
     /// <param name="childNum"></param>
     /// <returns>決定された箱庭の配列</returns>
-    private List<GameObject> SetRandomGarden(int childNum)
+    private GameObject[] SetRandomGarden(int childNum)
     {
-        // マップの箱庭を決定
-        int gardenSetNum = _waveNum + 1;// 何個の箱庭を生成するか +1は準備の箱庭
-        List<GameObject> gardens = new List<GameObject>();
-        // waveの個数分
-        for (int i = 1; i < _waveNum; i++)// _waveNum分ループする
+        GameObject[] gardens = new GameObject[_waveNum+1];// wave + 準備箱庭
+        for (int i = 0; i < _waveNum; i++)// _waveNum分ループする
         {
             int index = UnityEngine.Random.Range(0, childNum);//  using:Systemと曖昧だったためUnityEngineを付けています
             gardens[i] = _gardenKinds[index];
         }
 
         // 最後に箱庭を追加
-        gardens[gardenSetNum - 1] = _prepareGarden;
+        gardens[_waveNum] = _prepareGarden;
 
         return gardens;
     }
-    private Vector3 SetGardenPos(GameObject[] gardenSet)
-    {
-
-
-        return new Vector3(0,0,0);
-    }
-    /*
-    /// <summary>
-    /// 箱庭生成でマップを作る
-    /// </summary>
-    /// <param name="gardenSet"></param>
-    private async UniTaskVoid GenerateGarden(GameObject[] gardenSet)
-    {
-        float gardenHalfScalZ = gardenSet[idx].transform.localScale.z * 10 / 2;// 箱庭の奥行の半分長さを取る メッシュによって変わるので変更が必要
-        // 箱庭生成
-        Instantiate(gardenSet[idx], _gardenPos, Quaternion.Euler(Vector3.zero));// i番目の箱庭を生成　回転はゼロ
-        // await UniTask.WaitUntil(()=> );// prepereGardenの場所まで到達したら開始
-        // 次の箱庭の位置を算出
-        float nextGardenHalfScalZ = 0;
-
-        try
-        {
-            nextGardenHalfScalZ = gardenSet[++idx].transform.localScale.z * 10 / 2;
-        }
-        catch (IndexOutOfRangeException)
-        {
-            _gardenPos += new Vector3(0, 0, gardenHalfScalZ);// 位置の決定
-            return;
-        }
-
-        float gardenPosZ = gardenHalfScalZ + nextGardenHalfScalZ;// 合計
-        _gardenPos += new Vector3(0, 0, gardenPosZ);// 位置の決定
-    }
-    */
-    void WaveCount()
-    {
-
-    }
 }
-/*
-public class Wave : MonoBehaviour
-{
-
-}
-public class RandomGenerat : MonoBehaviour
-{
-    int generateNum = 1;
-    Vector3 pos = Vector3.zero;
-    RandomGenerat(GameObject[] _garden)
-    {
-        int index = Random.Range(0, _garden.Length);
-        GameObject generateObj = _garden[index];
-        Instantiate(generateObj, pos, Quaternion.identity);
-        generateNum++;
-    }
-}
-
-/*
-// 生成手順
-
-// 初回生成
-int index = Random.Range(0, _garden.Length);
-GameObject generateObj = _garden[index];
-Instantiate(generateObj, pos, Quaternion.identity);
-
-int generateNum = 2;
-while ()
-{
-    int generateCount = 1;
-    while (generateNum <= generateCount)
-    {
-        // 今回生成したオブジェクトの半分を足す
-        float objScaleZ = generateObj.transform.localScale.z * 10;
-        pos += new Vector3(0, 0, objScaleZ);
-
-        index = Random.Range(0, _garden.Length);
-        generateObj = _garden[index];
-
-        objScaleZ = generateObj.transform.localScale.z * 10;
-        pos += new Vector3(0, 0, objScaleZ);
-
-        generateCount++;
-    }
-}
-
-
-
-/*do
-{
-    // 生成するオブジェクトを決める
-    index = Random.Range(0, _garden.Length);
-    generateObj = _garden[index];
-
-    // 生成
-    Instantiate(generateObj, pos, Quaternion.identity);
-
-    // 今回生成したオブジェクトの半分を足す
-    float objScaleZ = generateObj.transform.localScale.z * 10;
-    pos += new Vector3(0, 0, objScaleZ);
-    // 次回生成したオブジェクトの半分を足す
-    // 生成
-}
-while (_garden == null);
-
-foreach (GameObject child in _garden)
-{
-    Instantiate(child, pos, Quaternion.identity);
-
-    float objScaleZ = child.transform.localScale.z * 10;
-    pos += new Vector3(0,0,objScaleZ);
-}*/
